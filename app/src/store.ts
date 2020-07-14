124b0021dacc2576cf7ec27697a059b95e53b296
haku-noir <hakunoir.00@gmail.com>
2020-07-14 21:05:07 +0900

    add: storeにデータの実体を管理するentiresReducerを追加

--- app/src/store.ts
+++ app/src/store.ts
@@ -4,14 +4,17 @@ import {
 import { connectRouter, routerMiddleware, RouterState } from 'connected-react-router';
 import { History } from 'history';
 import logger from 'redux-logger';
+import { EntitiesState, entitiesReducer } from 'reducers/entitiesReducer';
 
 export type RootState = {
   router: RouterState,
+  entires: EntitiesState
 };
 
 export const createStore = (history: History) => {
   const rootReducer = combineReducers<RootState>({
     router: connectRouter(history),
+    entires: entitiesReducer
   });
 
   return reduxCreateStore(
