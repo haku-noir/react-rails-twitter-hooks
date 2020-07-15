87d20a220a6176ca03539f2c2b703039459f8de4
haku-noir <hakunoir.00@gmail.com>
2020-07-15 19:52:09 +0900

    add: ツイートを表示する機能の追加

--- app/src/components/TweetList.tsx
+++ app/src/components/TweetList.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import { EntitiesState } from 'reducers/entitiesReducer';
 
-type IProps = {};
+type IProps = {
+  tweets: EntitiesState["tweets"]
+};
 
 export const TweetList: React.FC<IProps> = (props: IProps) => {
+  const { tweets } = props;
+
   return (
-    <div>TweetList</div>
+    <div>
+      <h1>TweetList</h1>
+      <ul>
+        {tweets.map(tweet => (
+          <p>{tweet.content}</p>
+        ))}
+      </ul>
+    </div>
   );
 };
--- app/src/containers/TweetListCTR.tsx
+++ app/src/containers/TweetListCTR.tsx
@@ -3,15 +3,18 @@ import { Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { TweetList as TweetListComp } from 'components/TweetList';
 import { RootState } from 'store';
+import { EntitiesState } from 'reducers/entitiesReducer';
 
 type IProps = {};
 
 export const TweetList: React.FC<IProps> = (props: IProps) => {
   const dispatch = useDispatch<Action>();
 
-  const state = useSelector<RootState, RootState>(state => state);
+  const tweets = useSelector<RootState, EntitiesState["tweets"]>(
+    state => state.entires.tweets
+  );
 
-  const _props = {}
+  const _props = { tweets }
 
   return <TweetListComp {..._props}/>;
 };
