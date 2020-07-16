06f62e4507ae356feb578b0846acb535a60d8d27
haku-noir <hakunoir.00@gmail.com>
2020-07-16 18:44:48 +0900

    update: material-uiによるTweetListコンポーネントの更新

--- app/src/components/TweetList.tsx
+++ app/src/components/TweetList.tsx
@@ -1,21 +1,58 @@
 import * as React from 'react';
 import { EntitiesState } from 'reducers/entitiesReducer';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 type IProps = {
   tweets: EntitiesState["tweets"]
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: '36ch',
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    },
+  }),
+);
+
 export const TweetList: React.FC<IProps> = (props: IProps) => {
   const { tweets } = props;
+  const classes = useStyles();
 
   return (
-    <div>
-      <h1>TweetList</h1>
-      <ul>
-        {tweets.map(tweet => (
-          <p>{tweet.content}</p>
-        ))}
-      </ul>
-    </div>
+    <List className={classes.root}>
+      {tweets.map(tweet => (
+        <ListItem key={tweet.id} alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Anonymous" src="" />
+          </ListItemAvatar>
+          <ListItemText
+            primary={tweet.content}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Anonymous
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      ))}
+    </List>
   );
 };
