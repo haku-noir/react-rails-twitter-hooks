38b7b93c7698bb9711286a44c89be13165ab55f5
haku-noir <hakunoir.00@gmail.com>
2020-07-22 04:41:37 +0900

    add: ツイートをapiへpost送信するクライアントの追加

--- app/src/clients/tweetsAPI.ts
+++ app/src/clients/tweetsAPI.ts
@@ -1,7 +1,20 @@
+import { TweetState } from "reducers/entitiesReducer";
+
 const baseURL = 'http://localhost';
 
 export const fetchTweets = () => fetch(`${baseURL}/tweets`, {
   method: 'GET',
   mode: 'cors',
   credentials: 'include',
+});
+
+export const sendTweet = (tweet: TweetState) => fetch(`${baseURL}/tweets`, {
+  method: 'POST',
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(tweet),
 });
