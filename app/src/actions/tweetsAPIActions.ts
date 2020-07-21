8070a7928ecee3313ea7179d40027d2d163350c7
haku-noir <hakunoir.00@gmail.com>
2020-07-22 04:53:17 +0900

    add: tweetsAPIActionsにツイート送信用のアクションを追加

--- app/src/actions/tweetsAPIActions.ts
+++ app/src/actions/tweetsAPIActions.ts
@@ -1,8 +1,10 @@
 import actionCreatorFactory from 'typescript-fsa';
+import { TweetState } from 'reducers/entitiesReducer';
 
 const actionCreator = actionCreatorFactory();
 
 export const tweetsAPIActions = {
   updateTweets: actionCreator<void>('API_UPDATE_TWEETS'),
   updateTweetsDone: actionCreator<void>('API_UPDATE_TWEETS_DONE'),
+  sendTweet: actionCreator<TweetState>('API_SEND_TWEET'),
 };
--- app/src/reducers/tweetsAPIReducer.ts
+++ app/src/reducers/tweetsAPIReducer.ts
@@ -1,12 +1,18 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { tweetsAPIActions } from 'actions/tweetsAPIActions';
+import { TweetState } from './entitiesReducer';
 
 export type TweetsAPIState = {
-  update: boolean
+  update: boolean,
+  newTweet: TweetState,
 };
 
 const initialState: TweetsAPIState = {
   update: true,
+  newTweet: {
+    id: 0,
+    content: ''
+  },
 };
 
 export const tweetsAPIReducer = reducerWithInitialState(initialState)
@@ -17,4 +23,8 @@ export const tweetsAPIReducer = reducerWithInitialState(initialState)
   .case(tweetsAPIActions.updateTweetsDone, (state: TweetsAPIState): TweetsAPIState => ({
     ...state,
     update: false,
+  }))
+  .case(tweetsAPIActions.sendTweet, (state: TweetsAPIState, payload: TweetState): TweetsAPIState => ({
+    ...state,
+    newTweet: payload
   }));
