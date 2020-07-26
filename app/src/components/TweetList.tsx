4c14a520c4a14588f81e1dde60c5c6e1d135415d
haku-noir <hakunoir.00@gmail.com>
2020-07-27 02:02:26 +0900

    change: TweetListがツイートのentryIDの配列であるtweets.homeを利用するように変更

--- app/src/components/TweetList.tsx
+++ app/src/components/TweetList.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { EntitiesState } from 'reducers/entitiesReducer';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { Tweet } from './Tweet';
 
 type IProps = {
-  tweets: EntitiesState["tweets"]
+  tweetIds: number[]
 };
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -20,14 +19,14 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export const TweetList: React.FC<IProps> = (props: IProps) => {
-  const { tweets } = props;
+  const { tweetIds } = props;
   const classes = useStyles();
 
   return (
     <List className={classes.root}>
-      {tweets.map((tweet) => (
-        <ListItem key={tweet.id} alignItems="flex-start">
-          <Tweet tweet={tweet} />
+      {tweetIds.map((tweetId) => (
+        <ListItem key={tweetId} alignItems="flex-start">
+          <Tweet tweetId={tweetId} />
         </ListItem>
       ))}
     </List>
--- app/src/containers/TweetListCTR.tsx
+++ app/src/containers/TweetListCTR.tsx
@@ -3,18 +3,18 @@ import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { TweetList as TweetListComp } from 'components/TweetList';
 import { RootState } from 'store';
-import { EntitiesState } from 'reducers/entitiesReducer';
+import { TweetsState } from 'reducers/tweetsReducer';
 
 type IProps = {};
 
 export const TweetList: React.FC<IProps> = (props: IProps) => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const tweets = useSelector<RootState, EntitiesState["tweets"]>(
-    state => state.entires.tweets
+  const tweetIds = useSelector<RootState, TweetsState["home"]>(
+    state => state.tweets.home
   );
 
-  const _props = { tweets }
+  const _props = { tweetIds }
 
   return <TweetListComp {..._props}/>;
 };
