856c6512915b3e70c2c74a0e9dd8ea95f94c0db7
haku-noir <hakunoir.00@gmail.com>
2020-08-05 06:24:52 +0900

    add: UpdateTweetAPIの追加

---
+++ app/src/api/UpdateTweetAPI.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Dispatch, Action } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'store';
+import { TweetState } from 'reducers/entitiesReducer';
+import { updateTweet } from 'clients/tweetsAPI';
+import { tweetsAPIActions } from 'actions/tweetsAPIActions';
+
+export const UpdateTweetAPI: React.FC<{}> = () => {
+  const dispatch = useDispatch<Dispatch<Action>>();
+
+  const updatedTweet = useSelector<RootState, TweetState>(
+    state => state.tweetsAPI.updatedTweet
+  );
+
+  React.useEffect(() => {
+    if(updatedTweet.id === 0) return;
+
+    updateTweet(updatedTweet)
+    .then(() => {
+      dispatch(tweetsAPIActions.updateTweets());
+    })
+    .then(() => {
+      dispatch(tweetsAPIActions.updateTweetDone());
+    })
+    .catch(() => {
+      dispatch(tweetsAPIActions.updateTweetDone());
+    });
+  }, [updatedTweet]);
+
+  return null;
+};
