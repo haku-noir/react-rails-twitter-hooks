a0d0586c132e8bc64b5bc628476478aa5c6a44e5
haku-noir <hakunoir.00@gmail.com>
2020-08-08 00:53:19 +0900

    fix: アクション名を修正

--- app/src/actions/tweetsAPIActions.ts
+++ app/src/actions/tweetsAPIActions.ts
@@ -10,6 +10,6 @@ export const tweetsAPIActions = {
   sendTweetDone: actionCreator<void>('API_SEND_TWEET_DONE'),
   updateTweet: actionCreator<TweetState>('API_UPDATE_TWEET'),
   updateTweetDone: actionCreator<void>('API_UPDATE_TWEET_DONE'),
-  deleteTweet: actionCreator<number>('API_SEND_TWEET'),
-  deleteTweetDone: actionCreator<void>('API_SEND_TWEET_DONE'),
+  deleteTweet: actionCreator<number>('API_DELETE_TWEET'),
+  deleteTweetDone: actionCreator<void>('API_DELETE_TWEET_DONE'),
 };
