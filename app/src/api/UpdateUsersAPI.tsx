dbd04e1716a0c981eed0d5b2bdb22f25e924bbc6
haku-noir <hakunoir.00@gmail.com>
2020-08-11 11:59:47 +0900

    add: UpdateUsersAPIにapiからユーザを読み込む機能の追加

--- app/src/api/UpdateUsersAPI.tsx
+++ app/src/api/UpdateUsersAPI.tsx
@@ -2,15 +2,33 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { fetchUsers } from 'clients/usersAPI';
+import { entitiesActions } from 'actions/entitiesActions';
+import { usersAPIActions } from 'actions/usersAPIActions';
 
 export const UpdateUsersAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const updating = useSelector<RootState, boolean>(
+    state => state.usersAPI.updating
   );
 
-  React.useEffect(() => {}, [data]);
+  React.useEffect(() => {
+    if(!updating) return;
+
+    fetchUsers()
+      .then(res => res.json())
+      .then(res => {
+        if(!res.users) return;
+        dispatch(entitiesActions.updateUsers(res.users));
+      })
+      .then(() => {
+        dispatch(usersAPIActions.updateUsersDone());
+      })
+      .catch(() => {
+        dispatch(usersAPIActions.updateUsersDone());
+      });
+  }, [updating]);
 
   return null;
 };
