42b5fae78a6ff173074311d54a3af6b3f3c74ab2
haku-noir <hakunoir.00@gmail.com>
2020-08-13 19:22:41 +0900

    add: ユーザ一覧のentryIDの配列を抽出するUsersIndexExtractorを追加

--- app/src/extractor/UsersIndexExtractor.tsx
+++ app/src/extractor/UsersIndexExtractor.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { EntitiesState } from 'reducers/entitiesReducer';
+import { UsersState } from 'reducers/usersReducer';
+import { usersActions } from 'actions/usersActions';
 
-const extract = (data: RootState): RootState => (
-  data
+const extract = (users: EntitiesState["users"]): UsersState["index"] => (
+  users.map(user => user.id)
 );
 
 export const UsersIndexExtractor: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const users = useSelector<RootState, EntitiesState["users"]>(
+    state => state.entities.users
   );
 
   React.useEffect(() => {
-    const extractedData = extract(data);
-  }, [data]);
+    const index = extract(users);
+
+    dispatch(usersActions.updateIndex(index))
+  }, [users]);
 
   return null;
 };
