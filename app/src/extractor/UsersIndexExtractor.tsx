77fe369f4be52482536acbf80906a3a3cfdd4303
haku-noir <hakunoir.00@gmail.com>
2020-08-13 19:19:11 +0900

    chore: reactance e usersIndex

---
+++ app/src/extractor/UsersIndexExtractor.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Dispatch, Action } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'store';
+
+const extract = (data: RootState): RootState => (
+  data
+);
+
+export const UsersIndexExtractor: React.FC<{}> = () => {
+  const dispatch = useDispatch<Dispatch<Action>>();
+
+  const data = useSelector<RootState, RootState>(
+    state => state
+  );
+
+  React.useEffect(() => {
+    const extractedData = extract(data);
+  }, [data]);
+
+  return null;
+};
