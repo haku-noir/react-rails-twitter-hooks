ccafa7c1245af17c4e54075058f8038c5980b932
haku-noir <hakunoir.00@gmail.com>
2020-08-16 08:29:31 +0900

    add: usersAPIReducerにloginプロパティを追加

--- app/src/reducers/usersAPIReducer.ts
+++ app/src/reducers/usersAPIReducer.ts
@@ -1,12 +1,19 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { usersAPIActions } from 'actions/usersAPIActions';
+import { UserState } from 'reducers/entitiesReducer';
 
 export type UsersAPIState = {
   fetching: boolean,
+  login: UserState
 };
 
 const initialState: UsersAPIState = {
   fetching: true,
+  login: {
+    id: 0,
+    name: '',
+    password: ''
+  }
 };
 
 export const usersAPIReducer = reducerWithInitialState(initialState)
