a260bea39324f92dfb9fbc26d4cfc56fc9fbd24b
haku-noir <hakunoir.00@gmail.com>
2020-08-21 23:12:17 +0900

    add: LoginUserAPIの追加

--- app/src/api/LoginUserAPI.tsx
+++ app/src/api/LoginUserAPI.tsx
@@ -2,15 +2,34 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { loginUser } from 'clients/usersAPI';
+import { UserState } from 'reducers/entitiesReducer';
+import { usersAPIActions } from 'actions/usersAPIActions';
+import { usersActions } from 'actions/usersActions';
 
 export const LoginUserAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const loggedinUser = useSelector<RootState, UserState>(
+    state => state.usersAPI.loggedinUser
   );
 
-  React.useEffect(() => {}, [data]);
+  React.useEffect(() => {
+    if(!loggedinUser) return;
+
+    loginUser(loggedinUser)
+      .then(res => res.json())
+      .then(res => {
+        if(!res.user) return;
+        dispatch(usersActions.updateLogin(res.user));
+      })
+      .then(() => {
+        dispatch(usersAPIActions.loginUserDone());
+      })
+      .catch(() => {
+        dispatch(usersAPIActions.loginUserDone());
+      })
+  }, [loggedinUser]);
 
   return null;
 };
