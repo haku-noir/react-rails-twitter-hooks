0d6c08cdfbbf47b0c7543c9c8ad6d2feb74549c0
haku-noir <hakunoir.00@gmail.com>
2020-09-30 07:12:17 +0900

    add: RegisterUserAPIを追加

--- app/src/api/RegisterUserAPI.tsx
+++ app/src/api/RegisterUserAPI.tsx
@@ -2,15 +2,38 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { registerUser } from 'clients/usersAPI';
+import { UserState } from 'reducers/entitiesReducer';
+import { usersAPIActions } from 'actions/usersAPIActions';
+import { usersActions } from 'actions/usersActions';
+import { push } from 'connected-react-router';
 
 export const RegisterUserAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const registeredUser = useSelector<RootState, UserState>(
+    state => state.usersAPI.registeredUser
   );
 
-  React.useEffect(() => {}, [data]);
+  React.useEffect(() => {
+    if(!registeredUser.name || !registeredUser.password) return;
+
+    registerUser(registeredUser)
+      .then(res => res.json())
+      .then(res => {
+        if(!res.user) throw new Error('Faild');
+        dispatch(usersActions.updateLogin(res.user.id));
+      })
+      .then(() => {
+        dispatch(usersAPIActions.registerUserDone());
+      })
+      .then(() => {
+        dispatch(push('/'));
+      })
+      .catch(() => {
+        dispatch(usersAPIActions.registerUserDone());
+      })
+  }, [registeredUser]);
 
   return null;
 };
