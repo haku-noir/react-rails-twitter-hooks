d508c655d907e6807525538521d1ee9cfcf7cff5
haku-noir <hakunoir.00@gmail.com>
2020-10-06 06:08:46 +0900

    add: apiのtweetsテーブルにuser_idカラムを追加
    docker exec twitter_api bash -c "rails g migration add_user_id_to_tweets user_id:integer && rake db:migrate"

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_164352) do
+ActiveRecord::Schema.define(version: 2020_10_05_210751) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
