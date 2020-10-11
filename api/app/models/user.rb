090bd2af8c368585e5af64ce8402990f4c6ed9cb
haku-noir <hakunoir.00@gmail.com>
2020-10-12 05:35:20 +0900

    add: apiのUserにユーザが投稿したツイートのIDを全て返す関数を追加

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def tweets
+    tweets = Tweet.where(user_id: self.id).order(created_at: :desc)
+    tweet_ids = []
+
+    tweets.each do |tweet|
+      tweet_ids << tweet.id
+    end
+
+    return tweet_ids
+  end
+
 end
