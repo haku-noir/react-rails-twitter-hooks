24b1c93a302033598d9d237f30e934eba3d57d9b
haku-noir <hakunoir.00@gmail.com>
2020-10-19 05:54:04 +0900

    add: apiのusersにimage_nameカラムを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_210751) do
+ActiveRecord::Schema.define(version: 2020_10_18_205319) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_210751) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
