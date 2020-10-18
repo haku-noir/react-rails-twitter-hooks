060027122346d426002ce89c6a9bbda4ed43b245
haku-noir <hakunoir.00@gmail.com>
2020-10-19 05:27:04 +0900

    add: FetchLoginUserAPIの追加

--- app/src/api/FetchLoginUserAPI.tsx
+++ app/src/api/FetchLoginUserAPI.tsx
@@ -2,15 +2,33 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { fetchLoginUser } from 'clients/usersAPI';
+import { usersActions } from 'actions/usersActions';
+import { usersAPIActions } from 'actions/usersAPIActions';
 
 export const FetchLoginUserAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const fetching = useSelector<RootState, boolean>(
+    state => state.usersAPI.fetchingLoginUser
   );
 
-  React.useEffect(() => {}, [data]);
+  React.useEffect(() => {
+    if(!fetching) return;
+
+    fetchLoginUser()
+      .then(res => res.json())
+      .then(res => {
+        if(!res.user) return;
+        dispatch(usersActions.updateLogin(res.user.id));
+      })
+      .then(() => {
+        dispatch(usersAPIActions.fetchLoginUserDone());
+      })
+      .catch(() => {
+        dispatch(usersAPIActions.fetchLoginUserDone());
+      });
+  }, [fetching]);
 
   return null;
 };
