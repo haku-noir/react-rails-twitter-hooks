a6f0877379fea3a24328d359a2f376e8e016668b
haku-noir <hakunoir.00@gmail.com>
2020-10-20 06:19:49 +0900

    chore: reactance a UpdateUser

---
+++ app/src/api/UpdateUserAPI.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Dispatch, Action } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'store';
+
+export const UpdateUserAPI: React.FC<{}> = () => {
+  const dispatch = useDispatch<Dispatch<Action>>();
+
+  const data = useSelector<RootState, RootState>(
+    state => state
+  );
+
+  React.useEffect(() => {}, [data]);
+
+  return null;
+};
