bc55e05c11da2be479ae86881601a47a2fa3e718
haku-noir <hakunoir.00@gmail.com>
2020-10-20 06:08:30 +0900

    add: ユーザ情報の編集用クライアントを追加

--- app/src/clients/usersAPI.ts
+++ app/src/clients/usersAPI.ts
@@ -8,6 +8,17 @@ export const fetchUsers = () => fetch(`${baseURL}/users`, {
   credentials: 'include',
 });
 
+export const updateUser = (user: UserState) => fetch(`${baseURL}/users/${user.id}`, {
+  method: 'PUT',
+  mode: 'cors',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(user),
+});
+
 export const loginUser = (user: UserState) => fetch(`${baseURL}/login`, {
   method: 'POST',
   mode: 'cors',
