3778e358d0c9aa94743d4f4e9a1d11ec61ecacb7
haku-noir <hakunoir.00@gmail.com>
2020-10-21 13:20:32 +0900

    add: UpdateUserAPIの追加

--- app/src/api/UpdateUserAPI.tsx
+++ app/src/api/UpdateUserAPI.tsx
@@ -2,15 +2,31 @@ import * as React from 'react';
 import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
+import { UserState } from 'reducers/entitiesReducer';
+import { updateUser } from 'clients/usersAPI';
+import { usersAPIActions } from 'actions/usersAPIActions';
 
 export const UpdateUserAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(
-    state => state
+  const updatedUser = useSelector<RootState, UserState>(
+    state => state.usersAPI.updatedUser
   );
 
-  React.useEffect(() => {}, [data]);
+  React.useEffect(() => {
+    if(updatedUser.id === 0) return;
+
+    updateUser(updatedUser)
+      .then(() => {
+        dispatch(usersAPIActions.fetchUsers());
+      })
+      .then(() => {
+        dispatch(usersAPIActions.updateUserDone());
+      })
+      .catch(() => {
+        dispatch(usersAPIActions.updateUserDone());
+      });
+  }, [updatedUser]);
 
   return null;
 };
